866e7ef93f07d764de123206b8359d87f73086de
tanyj0612 <tanyijia9558@gmail.com>
2026-09-02 23:12:51 +0800

    remove : redo

--- lib/pages/map_view/home_page.dart
+++ lib/pages/map_view/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -113,73 +115,30 @@ class _HomePageState extends State<HomePage> {
             // Map Area
             // =========================
             Expanded(
-              child: Stack(
+              child: FlutterMap(
+                options: const MapOptions(
+                  initialCenter: LatLng(4.2105, 108.9758),
+                  initialZoom: 6,
+                ),
                 children: [
-
-                  // Temporary map
-                  Container(
-                    width: double.infinity,
-                    color: Colors.grey.shade300,
-
-                    child: const Center(
-                      child: Text(
-                        'MAP AREA',
-                        style: TextStyle(
-                          fontSize: 26,
-                          fontWeight: FontWeight.bold,
+                  TileLayer(
+                    urlTemplate:
+                    'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                    userAgentPackageName: 'com.example.myapp',
+                  ),
+                  MarkerLayer(
+                      markers:[
+                        Marker(
+                            point: LatLng(3.1390, 101.6869),
+                            width:40,
+                            height: 40,
+                            child:  const Icon(
+                              Icons.location_on,
+                              color: Colors.red,
+                              size: 40,
+                            ),
                         ),
-                      ),
-                    ),
-                  ),
-
-                  // Example restaurant marker
-                  Positioned(
-                    left: 110,
-                    top: 120,
-                    child: _buildMarker(
-                      rating: '3.5',
-                      name: 'Omer Yusel',
-                    ),
-                  ),
-
-                  Positioned(
-                    left: 100,
-                    top: 155,
-                    child: _buildMarker(
-                      rating: '3.6',
-                      name: 'La Bonne',
-                    ),
-                  ),
-
-                  Positioned(
-                    left: 130,
-                    bottom: 130,
-                    child: _buildMarker(
-                      rating: '4.3',
-                      name: 'Pizzeria Capricciosa',
-                    ),
-                  ),
-
-                  // Layer button
-                  Positioned(
-                    right: 15,
-                    top: 15,
-
-                    child: _buildCircleButton(
-                      icon: Icons.layers_outlined,
-                      onPressed: () {},
-                    ),
-                  ),
-
-                  // Current location button
-                  Positioned(
-                    right: 15,
-                    bottom: 15,
-
-                    child: _buildCircleButton(
-                      icon: Icons.navigation,
-                      onPressed: () {},
-                    ),
+                      ]
                   ),
                 ],
               ),
